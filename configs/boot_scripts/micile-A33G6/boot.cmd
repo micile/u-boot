f5ca8e599b63c2fe4af01e582af1aeb97a648f3f
Lawrence Yu <lyu@micile.com>
2016-10-02 21:56:18 -0700

    fix broken line endings in boot.cmd

--- configs/boot_scripts/micile-A33G6/boot.cmd
+++ configs/boot_scripts/micile-A33G6/boot.cmd
@@ -2,3 +2,4 @@ setenv bootargs console=tty0 console=ttyS0,115200 earlyprintk rw no_console_susp
 load mmc 0:1 0x43000000 micile-A33G6.dtb
 load mmc 0:1 0x42000000 zImage
 bootz 0x42000000 - 0x43000000
+
