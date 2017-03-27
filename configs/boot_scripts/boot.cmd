b1763ec905e5ec3e0f66e6f07da24ba85dfb79bc
Lawrence Yu <lyu@micile.com>
2017-03-26 21:13:01 -0700

    change boot cmd to ignore unused clocks.

--- configs/boot_scripts/boot.cmd
+++ configs/boot_scripts/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=tty0 console=ttyS0,115200 earlyprintk rw no_console_suspend=1 root=/dev/mmcblk0p2 rootwait panic=10 ${extra}
+setenv bootargs console=tty0 console=ttyS0,115200 earlyprintk rw no_console_suspend=1 root=/dev/mmcblk0p2 rootwait panic=10 ${extra} clk_ignore_unused
 load mmc 0:1 0x43000000 micile.dtb
 load mmc 0:1 0x42000000 zImage
 bootz 0x42000000 - 0x43000000
